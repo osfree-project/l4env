03e5c7857e5e2a999de895b9727d8c21be5e699e
l4check <l4check@a704ac0b-3a55-4d43-a2a9-7be6f07c34fb>
2008-02-23 03:52:22 +0000

    update: sync
    git-svn-id: http://svn.tudos.org/repos/tudos/trunk@302 a704ac0b-3a55-4d43-a2a9-7be6f07c34fb

--- l4/pkg/flips/server/lib-linux/include/linux/proc_fs.h
+++
@@ -1,8 +0,0 @@
-#include_next <linux/proc_fs.h>
-
-/* XXX prevent compilation error produced by unconditionally using
- * "proc_net_stat" in net/ipv4/route.c *grmpf*
- */
-#ifndef CONFIG_PROC_FS
-extern struct proc_dir_entry *proc_net_stat;
-#endif
