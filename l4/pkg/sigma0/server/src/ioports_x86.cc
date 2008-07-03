adef76fd3f116e75179899a8925aada174a0217c
l4check <l4check@a704ac0b-3a55-4d43-a2a9-7be6f07c34fb>
2008-07-03 02:51:09 +0000

    update
    git-svn-id: http://svn.tudos.org/repos/tudos/trunk@383 a704ac0b-3a55-4d43-a2a9-7be6f07c34fb

--- l4/pkg/sigma0/server/src/ioports_x86.cc
+++ l4/pkg/sigma0/server/src/ioports_x86.cc
@@ -7,11 +7,13 @@
 
 #include <l4/cxx/iostream.h>
 
+enum { PORT_SHIFT = 12 };
+
 static Mem_man io_ports;
 
 void init_io_ports(l4_kernel_info_t *info)
 {
-  io_ports.add_free(Region::kr(0, 64*1024));
+  io_ports.add_free(Region::kr(0, (64*1024) << PORT_SHIFT));
 }
 
 void dump_io_ports()
@@ -25,14 +27,14 @@ bool handle_io_page_fault(unsigned long d1, unsigned long d2, unsigned owner,
 {
   if (l4_is_io_page_fault(d1))
     {
-      unsigned port, size;
-      port = ((l4_fpage_t&)d1).iofp.iopage;
-      size = ((l4_fpage_t&)d1).iofp.iosize;
+      unsigned long port, size;
+      port = ((l4_fpage_t&)d1).iofp.iopage << PORT_SHIFT;
+      size = ((l4_fpage_t&)d1).iofp.iosize + PORT_SHIFT;
 
-      unsigned long i = io_ports.alloc(Region::bs(port, 1 << size, owner));
+      unsigned long i = io_ports.alloc(Region::bs(port, 1UL << size, owner));
       if (i == port)
 	{
-	  rd2 = l4_iofpage(port, size, 0).fpage;
+	  rd2 = l4_iofpage(port >> PORT_SHIFT, size - PORT_SHIFT, 0).fpage;
 	  rd1 = 0;
 	  msg = L4_IPC_SHORT_FPAGE;
 	}
